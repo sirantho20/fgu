c43e3a919f08f1ebcc24acaf42ad7af33e19d193
aafetsrom <aafetsrom@HTG-LP-59.hta.local>
2014-07-18 18:43:01 +0000

    fgu reporting preparation

--- console/migrations/m140718_174349_dataUpload.php
+++ console/migrations/m140718_174349_dataUpload.php
@@ -14,13 +14,7 @@ class m140718_174349_dataUpload extends Migration
         $this->execute('delete from gensets');
         $this->execute('delete from utility_meter');
         $this->execute('delete from genset_cph');
-        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/site.sql'));
-        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/gensets.sql'));
-        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/utility_meter.sql'));
-        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/meter_site.sql'));
-        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/site_genset.sql'));
-        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/site_details.sql'));
-        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/genset_cph.sql'));
+        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/fgu_data.sql'));
     }
 
     public function safeDown()
