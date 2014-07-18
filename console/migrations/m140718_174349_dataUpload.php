11ebca94f1e7075e9a119cf548985ed97cc89b3f
aafetsrom <aafetsrom@HTG-LP-59.hta.local>
2014-07-18 17:57:01 +0000

    fgu reporting preparation

---
+++ console/migrations/m140718_174349_dataUpload.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m140718_174349_dataUpload extends Migration
+{
+    public function safeUp()
+    {
+        $this->execute('delete from site_details');
+        $this->execute('delete from site_genset');
+        $this->execute('delete from meter_site');
+        $this->execute('delete from site');
+        $this->execute('delete from gensets');
+        $this->execute('delete from utility_meter');
+        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/site.sql'));
+        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/gensets.sql'));
+        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/utility_meter.sql'));
+        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/meter_site.sql'));
+        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/site_genset.sql'));
+        $this->execute(file_get_contents(\Yii::getAlias('@common').'/data/htg/site_details.sql'));
+    }
+
+    public function safeDown()
+    {
+        $this->execute('delete from site_details');
+        $this->execute('delete from site_genset');
+        $this->execute('delete from meter_site');
+        $this->execute('delete from site');
+        $this->execute('delete from gensets');
+        $this->execute('delete from utility_meter');
+    }
+}
