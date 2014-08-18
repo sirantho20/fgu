9e4680c99ee9fec4b4472172459d6a560956a147
tony <tony@dev>
2014-08-18 16:19:35 +0000

    validation and cleanups

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -58,13 +58,6 @@ AppAsset::register($this);
         </div>
     </div>
 
-    <footer class="footer">
-        <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
-        </div>
-    </footer>
-
     <?php $this->endBody() ?>
 </body>
 </html>
